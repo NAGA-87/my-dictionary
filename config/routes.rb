0ec9718fc11cc70cc38b440467fb3c85f1125b1b
NAGA-87 <naga.2000.chr5@gmail.com>
2021-02-10 18:37:36 +0900

    ユーザー情報編集機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'words/index'
   root to: "words#index"
+  resources :users, only: [:edit, :update]
   # devise_scope :user do
   # root :to => 'devise/sessions#session'
   # end
-  # references :words, only: :index
 end
